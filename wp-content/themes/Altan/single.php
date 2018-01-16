2d2355e5f04dfb43aa521ce91e5f2e596fef0211
ulises flores <ulises@estrategasdigitales.com>
2018-01-16 14:16:00 -0600

    Share plugin

--- wp-content/themes/Altan/single.php
+++ wp-content/themes/Altan/single.php
@@ -9,7 +9,7 @@ body {
 <div class="container" style="margin-top:50px;margin-bottom:50px">
 	<div class="row">
 		<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-			<?php echo do_shortcode('[ssba-buttons]'); ?>
+			<?php echo do_shortcode('[addtoany]'); ?>
 		</div>
 		<div class="col-lg-8 col-md-8 col-sm-7 col-xs-12 contenido-single" style="background-color:#ffffff">
 			<p class="regresar"><a href="<?php echo get_site_url(); ?>/category/sala-de-prensa/boletines/">REGRESAR A BOLETINES</a></p>
