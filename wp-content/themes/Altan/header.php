51fc5891a7a051e813bbcdcaf89d6d3133c00c9b
ulises flores <ulises@estrategasdigitales.com>
2018-01-16 15:37:01 -0600

    Avance responsivo

--- wp-content/themes/Altan/header.php
+++ wp-content/themes/Altan/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width user-scalable=no" />
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
